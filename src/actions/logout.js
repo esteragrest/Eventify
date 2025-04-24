769e1f208c949d48482b1810cfea0e90d1697aee
Провальская Дарья <provalskayadaria@gmail.com>
2025-04-24 18:59:23 +0300

    Refactoring: fixed the route to log out of your account

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -2,7 +2,7 @@ import { request } from "../utils"
 import { ACTION_TYPE } from "./action-type"
 
 export const logout = () => {
-	request('/auth/logout', 'POST')
+	request('/api/auth/logout', 'POST')
 	return {
 		type: ACTION_TYPE.LOGOUT
 	}
